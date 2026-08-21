39ef84b27f96074c3bc8a2434c6378d03a1713c7
Ocean Guardian <charanteja.kondakalla030206@gmail.com>
2026-08-21 10:59:05 +0530

    added 5th program

---
+++ prog5.dart
@@ -0,0 +1,10 @@
+void main(){
+    int age = 17 ;
+    if (age <= 12 && age > 0 )
+    print('Child');
+    else if (age>= 13 && age <=18)
+    print('Teenager');
+    else 
+    print('Adult');
+
+}
