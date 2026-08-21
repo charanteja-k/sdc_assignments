910dbf6fb30f660252faef1a27d3fdbc22616795
Ocean Guardian <charanteja.kondakalla030206@gmail.com>
2026-08-21 11:02:06 +0530

    added 6th program

---
+++ prog6.dart
@@ -0,0 +1,12 @@
+void main(){
+    int a=10,b=12,c=13;
+    if (a>b && a>c){
+        print('a is greater');
+    }
+    else if (b>a && b>c){
+        print('b is greater');
+    }
+    else{
+        print('c is greater');
+    }
+}
